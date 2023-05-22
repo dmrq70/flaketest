0117ae5480b1c464c36b6c749046b917dfb140f8
Diego Reyes Quintana <dreyes@bbva.com>
2023-05-22 18:06:27 -0300

    Todos los servers en ssh_config

--- home-manager/apps/ssh.nix
+++ home-manager/apps/ssh.nix
@@ -7,7 +7,7 @@
     matchBlocks = {
       "dreyes" = {
         hostname = "dreyes.robin-major.ts.net";
-	dynamicForwards = [{ port = 9060; }];
+	      dynamicForwards = [{ port = 9060; }];
       };
       "dreyes-ovpn" = {
         hostname= "10.20.0.3";
@@ -15,17 +15,37 @@
       };
       "tecinf4" = {
         hostname = "tecinf4";
-	user = "root";
-	proxyJump= "dreyes";
-	identityFile = "~/.ssh/id_rsa-bbva";
+	      user = "root";
+	      proxyJump= "dreyes";
+	      identityFile = "~/.ssh/id_rsa-bbva";
       };
       "tecinf" = {
         hostname = "tecinf";
-	user = "root";
-	proxyJump= "dreyes";
-	identityFile = "~/.ssh/id_rsa-bbva";
+	      user = "root";
+	      proxyJump= "dreyes";
+	      identityFile = "~/.ssh/id_rsa-bbva";
       };
+      "metricas" = {
+        hostname =  "pvi115-metricas";
+        user =  "root";
+        proxyJump =  "dreyes";
+        identityFile =  "~/.ssh/id_ed25519-bbva";
+        localForward =  "3000 76.252.94.55:3000";
+      }; 
+      "gitlab.motorc.com.ar" = {
+        port =  2022;
+      };
+      
+      "beacon" = {
+        user =  "ubuntu";
+        hostname =  "beacon.robin-major.ts.net";
+        identityFile =  "~/.ssh/ssh-key-beacon.key";
+      };  
 
-    };
-  };	       
+      "ipacs" = {
+        user =  "administrador";
+        hostname =  "10.20.0.7";
+        dynamicForwards = [{port= "9060";}];  
+      };
+    };	       
 }
