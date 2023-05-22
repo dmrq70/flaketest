7c570be38961124f8f6ceefc8d6d81ad31836b7d
Diego Reyes Quintana <dreyes@bbva.com>
2023-05-22 18:25:35 -0300

    Las conf que encuentro

--- home-manager/apps/tmux.nix
+++ home-manager/apps/tmux.nix
@@ -6,7 +6,12 @@
 
     terminal = "screen-256color";
     escapeTime = 0;
-    repeatTime = 600;
+    #repeatTime = 600;
+
+    prefix = "C-space";
+    historyLimit = 5000;
+    clock24 = true;
+    
     
   };
 }
